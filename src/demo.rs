62c73c39195a9a8c164383e8e9a9e96593183d2e
Uncle Scientist <unclescientist@gmail.com>
2026-01-06 07:34:15 -0500

    demo puzzle, part 2

--- src/demo.rs
+++ src/demo.rs
@@ -1,8 +1,13 @@
 pub fn run() {
-    let part1 = std::fs::read_to_string("input/demo-1.txt")
+    let numbers = std::fs::read_to_string("input/demo.txt")
         .expect("missing file")
         .lines()
-        .map(|line| line.parse::<usize>().unwrap())
-        .sum::<usize>();
-    println!("Demo: part 1 = {part1}");
+        .map(|line| line.parse::<f64>().unwrap())
+        .collect::<Vec<_>>();
+
+    let sum = numbers.iter().sum::<f64>();
+    println!("Demo: part 1 = {sum}");
+
+    // let numbers = [11, 22, 30, 34, 48, 8, 57, 57, 69, 69, 69];
+    println!("Demo: part 2 = {}", (sum / numbers.len() as f64).round());
 }
