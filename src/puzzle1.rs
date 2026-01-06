6afd1af10df737e1a1c0083ab15767ac8b0e9369
Uncle Scientist <unclescientist@gmail.com>
2026-01-06 08:11:57 -0500

    Puzzle 1 - part 3

--- src/puzzle1.rs
+++ src/puzzle1.rs
@@ -1,18 +1,6 @@
 pub fn run() {
     let data = std::fs::read_to_string("input/puzzle-1.txt").expect("missing file");
-    let count = data
-        .lines()
-        .map(|line| {
-            line.chars()
-                .collect::<Vec<_>>()
-                .chunks(2)
-                .filter(|chunk| {
-                    (chunk[0] == 'b' && chunk[1] == 'a')
-                        || (chunk[0] == 'n' && (chunk[1] == 'a' || chunk[1] == 'e'))
-                })
-                .count()
-        })
-        .collect::<Vec<_>>();
+    let count = get_count(data.lines(), banane);
     println!("Puzzle 1, part 1 = {}", count.iter().sum::<usize>());
     println!(
         "Puzzle 1, part 2 = {}",
@@ -21,4 +9,21 @@ pub fn run() {
             .filter(|val| val.is_multiple_of(2))
             .sum::<usize>()
     );
+    let count = get_count(data.lines(), banana);
+    println!("Puzzle 1, part 3 = {}", count.iter().sum::<usize>());
+}
+
+fn banane(_chunk: &str) -> bool {
+    true
+}
+
+fn banana(chunk: &str) -> bool {
+    !chunk.contains("ne")
+}
+
+fn get_count(lines: std::str::Lines, filter: fn(&str) -> bool) -> Vec<usize> {
+    lines
+        .filter(|line| filter(line))
+        .map(|line| line.chars().collect::<Vec<_>>().chunks(2).count())
+        .collect::<Vec<_>>()
 }
