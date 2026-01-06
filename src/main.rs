4ff6f4e4ee777fd71db168959d8dd7ef7aa6456d
Uncle Scientist <unclescientist@gmail.com>
2026-01-06 08:44:13 -0500

    Puzzle 3, part 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod demo;
 mod puzzle1;
 mod puzzle2;
+mod puzzle3;
 
 fn main() {
     demo::run();
@@ -8,4 +9,6 @@ fn main() {
     puzzle1::run();
     println!("-");
     puzzle2::run();
+    println!("-");
+    puzzle3::run();
 }
---
+++ src/puzzle3.rs
@@ -0,0 +1,40 @@
+use std::{collections::HashMap, convert::Infallible, fmt::Display, str::FromStr};
+
+pub fn run() {
+    let data = std::fs::read_to_string("input/puzzle-3.txt").expect("missing file");
+    let colors: Vec<Color> = data.lines().map(|line| line.parse().unwrap()).collect();
+
+    let mut counts = HashMap::<Color, usize>::new();
+    for c in &colors {
+        *counts.entry(c.clone()).or_insert(0) += 1;
+    }
+    let mut count_list = counts.iter().collect::<Vec<_>>();
+    count_list.sort_by(|a, b| b.1.cmp(a.1));
+    println!("Puzzle 3, part 1 = {}", count_list[0].0);
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+struct Color {
+    red: i32,
+    green: i32,
+    blue: i32,
+}
+
+impl FromStr for Color {
+    type Err = Infallible;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let mut nums = line.split(',');
+        Ok(Self {
+            red: nums.next().unwrap().parse().unwrap(),
+            green: nums.next().unwrap().parse().unwrap(),
+            blue: nums.next().unwrap().parse().unwrap(),
+        })
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{},{},{}", self.red, self.green, self.blue)
+    }
+}
