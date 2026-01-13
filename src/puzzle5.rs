41032ef9347d355ec4f166fe656f17184c0c5a20
Uncle Scientist <unclescientist@gmail.com>
2026-01-13 07:25:56 -0500

    Puzzle 5, part 2

--- src/puzzle5.rs
+++ src/puzzle5.rs
@@ -1,11 +1,15 @@
+use std::collections::HashSet;
+
 pub fn run() {
     let data = std::fs::read_to_string("input/puzzle-5.txt").expect("file");
     let tunnels = data.trim().chars().collect::<Vec<_>>();
 
     let mut total_distance = 0;
     let mut index = 0;
+    let mut visited = HashSet::new();
     while index < tunnels.len() {
         let cur_char = tunnels[index];
+        visited.insert(cur_char);
         for (idx, ch) in tunnels.iter().enumerate() {
             if idx == index {
                 continue;
@@ -19,4 +23,12 @@ pub fn run() {
         index += 1;
     }
     println!("Puzzle 5, part 1 = {total_distance}");
+
+    let mut part2 = String::from("");
+    for ch in &tunnels {
+        if visited.insert(*ch) {
+            part2.push(*ch);
+        }
+    }
+    println!("Puzzle 5, part 2 = {part2}");
 }
