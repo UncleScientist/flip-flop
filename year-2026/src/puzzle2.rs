39319b76022b797f984342b336543b2192c93c2b
Uncle Scientist <unclescientist@gmail.com>
2026-07-07 07:40:38 -0400

    puzzle 2, part 2

--- year-2026/src/puzzle2.rs
+++ year-2026/src/puzzle2.rs
@@ -3,8 +3,10 @@ pub fn run() {
     let instructions = data.chars().map(Direction::get);
     let mut wall = Wall::default();
 
-    wall.apply(instructions);
-    println!("Puzzle 2, part 1 = {}", wall.part1());
+    wall.do_part_1(instructions.clone());
+    println!("Puzzle 2, part 1 = {}", wall.part1_score());
+
+    println!("Puzzle 2, part 1 = {}", wall.do_part_2(instructions));
 }
 
 #[derive(Debug)]
@@ -25,7 +27,7 @@ impl Wall {
         }
     }
 
-    fn apply(&mut self, instructions: impl Iterator<Item = Direction>) {
+    fn do_part_1(&mut self, instructions: impl Iterator<Item = Direction>) {
         let mut pos = 0;
         for dir in instructions {
             match dir {
@@ -36,7 +38,7 @@ impl Wall {
         }
     }
 
-    fn part1(&self) -> usize {
+    fn part1_score(&self) -> usize {
         let Some((index, max_val)) = self
             .segments
             .iter()
@@ -48,8 +50,32 @@ impl Wall {
         };
         (index + 1) * max_val
     }
+
+    fn do_part_2(&mut self, instructions: impl Iterator<Item = Direction>) -> usize {
+        let mut laser_pos = 0;
+        let mut robot_pos = 0;
+
+        let forward = instructions.collect::<Vec<Direction>>();
+        let reverse = forward.iter().rev().copied().collect::<Vec<Direction>>();
+
+        let mut count = 0;
+        for (laser, robot) in forward.iter().zip(reverse.iter()) {
+            match laser {
+                Direction::Left => laser_pos = (laser_pos + 99) % 100,
+                Direction::Right => laser_pos = (laser_pos + 1) % 100,
+            }
+            match robot {
+                Direction::Left => robot_pos = (robot_pos + 99) % 100,
+                Direction::Right => robot_pos = (robot_pos + 1) % 100,
+            }
+            count += (laser_pos == robot_pos) as usize;
+        }
+
+        count
+    }
 }
 
+#[derive(Copy, Clone)]
 enum Direction {
     Left,
     Right,
