7673ffac6bf6a42c61febe6b26c4f1755d8e161b
Uncle Scientist <unclescientist@gmail.com>
2026-07-10 07:07:03 -0400

    2026, puzzle 4, part 2

--- year-2026/src/puzzle4.rs
+++ year-2026/src/puzzle4.rs
@@ -2,9 +2,12 @@ use std::{convert::Infallible, str::FromStr};
 
 pub fn run() {
     let data = std::fs::read_to_string("input/puzzle-4.txt").expect("can't read file");
+    // let data = std::fs::read_to_string("test.txt").expect("can't read file");
 
     let flower = Flowerstalk::new(data.lines().map(|line| line.parse::<Branch>().unwrap()));
     println!("Puzzle 4, part 1 = {}", flower.count_leaves_above(400));
+
+    println!("Puzzle 4, part 2 = {}", flower.count_swaps());
 }
 
 #[derive(Debug)]
@@ -21,9 +24,30 @@ impl Flowerstalk {
             .filter(|block| matches!(block, Branch::Left | Branch::Right))
             .count()
     }
+
+    fn count_swaps(&self) -> usize {
+        let pos = self
+            .0
+            .iter()
+            .position(|branch| matches!(branch, Branch::Left | Branch::Right))
+            .expect("Need at least one branch in the puzzle input");
+        let mut cur_side = self.0[pos];
+        let mut swaps = 0;
+
+        for branch in &self.0[pos + 1..] {
+            if !matches!(branch, Branch::Left | Branch::Right) {
+                continue;
+            }
+            if *branch != cur_side {
+                swaps += 1;
+                cur_side = *branch;
+            }
+        }
+        swaps
+    }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 enum Branch {
     Left,
     Right,
