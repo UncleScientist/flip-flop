2a94c4e0b1e290f2df780c24e18f135ac71fd21d
Uncle Scientist <unclescientist@gmail.com>
2026-07-10 07:42:32 -0400

    2026, puzzle 5, part 2

--- year-2026/src/puzzle5.rs
+++ year-2026/src/puzzle5.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 pub fn run() {
     let data = std::fs::read_to_string("input/puzzle-5.txt").expect("missing file");
+    // let data = std::fs::read_to_string("test.txt").expect("missing file");
 
     let lines = data.lines().collect::<Vec<_>>();
 
@@ -10,10 +11,35 @@ pub fn run() {
         streets.push(line.chars().collect());
     }
 
-    println!("Puzzle 5, part 1 = {}", count_visited(&streets));
+    let baseline_visited = find_visited(&streets);
+    println!("Puzzle 5, part 1 = {}", baseline_visited.len());
+
+    let last_row = streets.len() - 1;
+    let last_col = streets[0].len() - 1;
+
+    let mut max_visited = baseline_visited.len();
+
+    for rotate in &baseline_visited {
+        if rotate.0 == 0 || rotate.0 == last_row || rotate.1 == 0 || rotate.1 == last_col {
+            continue;
+        }
+        let mut permute = streets.clone();
+        for _ in 0..3 {
+            permute[rotate.0][rotate.1] = match permute[rotate.0][rotate.1] {
+                '^' => '>',
+                '>' => 'v',
+                'v' => '<',
+                '<' => '^',
+                _ => panic!("compiler bug"),
+            };
+            let visited = find_visited(&permute).len();
+            max_visited = max_visited.max(visited);
+        }
+    }
+    println!("Puzzle 5, part 2 = {max_visited}");
 }
 
-fn count_visited(streets: &[Vec<char>]) -> usize {
+fn find_visited(streets: &[Vec<char>]) -> HashSet<(usize, usize)> {
     let mut pos = (0, 0);
     let mut visited = HashSet::new();
     visited.insert(pos);
@@ -32,5 +58,5 @@ fn count_visited(streets: &[Vec<char>]) -> usize {
         }
     }
 
-    visited.len()
+    visited
 }
