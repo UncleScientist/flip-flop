70bd11575a8d82b45babe03f9923273265dfb214
Uncle Scientist <unclescientist@gmail.com>
2026-07-13 07:45:44 -0400

    2026, puzzle 6, part 2

--- year-2026/src/puzzle6.rs
+++ year-2026/src/puzzle6.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashSet, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 pub fn run() {
     let data = std::fs::read_to_string("input/puzzle-6.txt").expect("file");
@@ -14,16 +14,22 @@ pub fn run() {
         gears.push(row);
     }
 
-    let mut grid = Grid { start, gears };
-    grid.rotate();
+    let mut grid = Grid::new(&gears, start);
+    grid.rotate_part_1();
     println!("Puzzle 6, part 1 = {}", grid.calculate_light_value());
+
+    let mut grid = Grid::new(&gears, start);
+    grid.rotate_part_2();
+    println!("Puzzle 6, part 2 = {}", grid.calculate_light_value());
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum Component {
     Start,
     Gear(Rotation),
     Light,
+    Input(char),
+    Output(char),
     Irrelevant,
 }
 
@@ -38,8 +44,10 @@ impl Component {
     fn translate(ch: char) -> Self {
         match ch {
             'S' => Self::Start,
-            '#' => Self::Gear(Rotation::Still),
+            '#' | '3' => Self::Gear(Rotation::Still),
             '*' => Self::Light,
+            'a'..='z' => Self::Input(ch),
+            'A'..='Z' => Self::Output(ch),
             _ => Self::Irrelevant,
         }
     }
@@ -48,10 +56,43 @@ impl Component {
 struct Grid {
     start: (usize, usize),
     gears: Vec<Vec<Component>>,
+    bluetooth: HashMap<(usize, usize), (usize, usize)>,
 }
 
 impl Grid {
-    fn rotate(&mut self) {
+    fn new(gears: &[Vec<Component>], start: (usize, usize)) -> Self {
+        let mut lc = HashMap::<char, (usize, usize)>::new();
+        let mut uc = HashMap::<char, (usize, usize)>::new();
+
+        for (r, row) in gears.iter().enumerate() {
+            for (c, comp) in row.iter().enumerate() {
+                match comp {
+                    Component::Input(ch) => {
+                        lc.insert(*ch, (r, c));
+                    }
+                    Component::Output(ch) => {
+                        uc.insert(*ch, (r, c));
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        let mut bluetooth = HashMap::new();
+        for (input, iloc) in lc {
+            let output = input.to_uppercase().to_string().chars().next().unwrap();
+            bluetooth.insert(iloc, uc[&output]);
+        }
+
+        println!("{bluetooth:?}");
+        Self {
+            start,
+            gears: gears.to_vec(),
+            bluetooth,
+        }
+    }
+
+    fn rotate_part_1(&mut self) {
         let mut rotated = HashSet::new();
         let mut queue = VecDeque::from([(self.start, Rotation::CounterClockwise)]);
 
@@ -82,6 +123,45 @@ impl Grid {
                     if matches!(self.gears[loc.0][loc.1], Component::Gear(_)) {
                         self.gears[loc.0][loc.1] = Component::Gear(opposite);
                         queue.push_back((loc, opposite));
+                    }
+                }
+            }
+        }
+    }
+
+    fn rotate_part_2(&mut self) {
+        let mut rotated = HashSet::new();
+        let mut queue = VecDeque::from([(self.start, Rotation::CounterClockwise)]);
+
+        self.gears[self.start.0][self.start.1] = Component::Gear(Rotation::CounterClockwise);
+
+        while let Some((pos, rot)) = queue.pop_front() {
+            let opposite = match rot {
+                Rotation::Still => panic!("prev gear at {pos:?} didn't move"),
+                Rotation::Clockwise => Rotation::CounterClockwise,
+                Rotation::CounterClockwise => Rotation::Clockwise,
+            };
+            if rotated.insert(pos) {
+                let mut check = Vec::new();
+                if pos.0 > 0 {
+                    check.push((pos.0 - 1, pos.1));
+                }
+                if pos.1 > 0 {
+                    check.push((pos.0, pos.1 - 1));
+                }
+                if pos.0 < self.gears.len() - 1 {
+                    check.push((pos.0 + 1, pos.1));
+                }
+                if pos.1 < self.gears[0].len() - 1 {
+                    check.push((pos.0, pos.1 + 1));
+                }
+
+                for loc in check {
+                    if matches!(self.gears[loc.0][loc.1], Component::Gear(_)) {
+                        self.gears[loc.0][loc.1] = Component::Gear(opposite);
+                        queue.push_back((loc, opposite));
+                    } else if let Some(btpos) = self.bluetooth.get(&loc) {
+                        queue.push_back((*btpos, rot));
                     }
                 }
             }
@@ -136,6 +216,8 @@ impl Grid {
                             Rotation::Clockwise => 'R',
                             Rotation::CounterClockwise => 'L',
                         },
+                        Component::Input(ch) => *ch,
+                        Component::Output(ch) => *ch,
                         Component::Light => '*',
                         Component::Irrelevant => '.',
                     }
