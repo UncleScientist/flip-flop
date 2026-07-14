7eb08095ea013e8dbe6dbd0ca853d58c5b55a69e
Uncle Scientist <unclescientist@gmail.com>
2026-07-14 07:11:15 -0400

    clean up asserts

--- year-2026/src/puzzle6.rs
+++ year-2026/src/puzzle6.rs
@@ -332,17 +332,17 @@ mod test {
 
     #[test]
     fn test_prime() {
-        assert_eq!(false, is_prime(0));
-        assert_eq!(false, is_prime(1));
-        assert_eq!(true, is_prime(2));
-        assert_eq!(true, is_prime(3));
-        assert_eq!(false, is_prime(4));
-        assert_eq!(true, is_prime(5));
-        assert_eq!(false, is_prime(6));
-        assert_eq!(true, is_prime(7));
-        assert_eq!(false, is_prime(8));
-        assert_eq!(false, is_prime(9));
-        assert_eq!(false, is_prime(10));
-        assert_eq!(true, is_prime(11));
+        assert!(!is_prime(0));
+        assert!(!is_prime(1));
+        assert!(is_prime(2));
+        assert!(is_prime(3));
+        assert!(!is_prime(4));
+        assert!(is_prime(5));
+        assert!(!is_prime(6));
+        assert!(is_prime(7));
+        assert!(!is_prime(8));
+        assert!(!is_prime(9));
+        assert!(!is_prime(10));
+        assert!(is_prime(11));
     }
 }
