5920026d99683332a68d33b6c19efc8fb5d56e05
Uncle Scientist <unclescientist@gmail.com>
2026-07-15 06:52:40 -0400

    2026, puzzle 9, part 2

--- year-2026/src/puzzle9.rs
+++ year-2026/src/puzzle9.rs
@@ -1,4 +1,7 @@
-use std::collections::{HashSet, VecDeque};
+use std::{
+    collections::{HashSet, VecDeque},
+    ops::{Add, AddAssign},
+};
 
 pub fn run() {
     // let data = std::fs::read_to_string("test.txt").expect("file");
@@ -45,6 +48,42 @@ pub fn run() {
             }
         }
     }
+
+    let mut visited = HashSet::new();
+    let mut queue = VecDeque::from([(start, 0)]);
+
+    while let Some((p, count)) = queue.pop_front() {
+        if p == end {
+            println!("Puzzle 9, part 1 = {count}");
+            break;
+        }
+        if visited.insert(p) {
+            for neighbor in p.neighbors().into_iter().chain(full_dist(&maze, &p)) {
+                if maze.contains(&neighbor) {
+                    continue;
+                }
+                if !visited.contains(&neighbor) {
+                    queue.push_back((neighbor, count + 1));
+                }
+            }
+        }
+    }
+}
+
+fn full_dist(maze: &HashSet<Point>, p: &Point) -> Vec<Point> {
+    let mut result = Vec::new();
+
+    for dir in &DIRS {
+        let mut start = *p;
+        while !maze.contains(&(start + *dir)) {
+            start += *dir;
+        }
+        if start != *p {
+            result.push(start);
+        }
+    }
+
+    result
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
@@ -58,5 +97,44 @@ impl Point {
             Point(self.0, self.1 - 1),
             Point(self.0, self.1 + 1),
         ]
+    }
+}
+
+const DIRS: [Direction; 4] = [
+    Direction::Up,
+    Direction::Down,
+    Direction::Left,
+    Direction::Right,
+];
+
+#[derive(Debug, Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl AddAssign<Direction> for Point {
+    fn add_assign(&mut self, rhs: Direction) {
+        *self = match rhs {
+            Direction::Up => Point(self.0 - 1, self.1),
+            Direction::Down => Point(self.0 + 1, self.1),
+            Direction::Left => Point(self.0, self.1 - 1),
+            Direction::Right => Point(self.0, self.1 + 1),
+        };
+    }
+}
+
+impl Add<Direction> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Direction) -> Self::Output {
+        match rhs {
+            Direction::Up => Point(self.0 - 1, self.1),
+            Direction::Down => Point(self.0 + 1, self.1),
+            Direction::Left => Point(self.0, self.1 - 1),
+            Direction::Right => Point(self.0, self.1 + 1),
+        }
     }
 }
