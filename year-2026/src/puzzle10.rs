9a2ea196fdc4729f5fcf949bf4dfe15bbba95565
Uncle Scientist <unclescientist@gmail.com>
2026-07-16 08:29:02 -0400

    2026, puzzle 10, part 3

--- year-2026/src/puzzle10.rs
+++ year-2026/src/puzzle10.rs
@@ -1,10 +1,29 @@
-use std::{convert::Infallible, str::FromStr};
+use std::{convert::Infallible, fmt::Display, str::FromStr};
 
 pub fn run() {
     let data = std::fs::read_to_string("input/puzzle-10.txt").expect("missing input file");
     let computer = Computer::new(&data.lines().collect::<Vec<&str>>());
     println!("Puzzle 10, part 1 = {}", computer.run()[0]);
     println!("Puzzle 10, part 2 = {}", computer.run_up_to_5000000());
+
+    /*
+    for (line_num, inst) in computer.instr.iter().enumerate() {
+        let mut label_found = false;
+        for (label_id, jump_loc) in computer.labels.iter().enumerate() {
+            if line_num == *jump_loc {
+                print!("L{label_id:<5} ");
+                label_found = true;
+                break;
+            }
+        }
+        if !label_found {
+            print!("       ");
+        }
+        println!("{inst}");
+    }
+    println!("---");
+    */
+    println!("Puzzle 10, part 3 = {}", computer.run_1048576());
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -20,6 +39,28 @@ enum Instruction {
     Jump(usize),
     Jeq(Reg, usize),
     Jne(Reg, usize),
+}
+
+impl Display for Instruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Instruction::Load(val, reg) => format!("load r{reg}, {val}"),
+                Instruction::Copy(src, dst) => format!("move r{dst}, r{src}"),
+                Instruction::Add(src1, src2, dst) => format!("add  r{dst}, r{src1}, r{src2}"),
+                Instruction::Sub(src1, src2, dst) => format!("sub  r{dst}, r{src1}, r{src2}"),
+                Instruction::Mul(src1, src2, dst) => format!("mul  r{dst}, r{src1}, r{src2}"),
+                Instruction::Mod(src1, src2, dst) => format!("mod  r{dst}, r{src1}, r{src2}"),
+                Instruction::Inc(reg) => format!("inc  r{reg}"),
+                Instruction::Dec(reg) => format!("dec  r{reg}"),
+                Instruction::Jump(dst) => format!("jmp  L{dst}"),
+                Instruction::Jeq(reg, dst) => format!("jeq  r{reg}, L{dst}"),
+                Instruction::Jne(reg, dst) => format!("jne  r{reg}, L{dst}"),
+            }
+        )
+    }
 }
 
 impl FromStr for Instruction {
@@ -49,6 +90,12 @@ impl FromStr for Instruction {
 #[derive(Debug, PartialEq, Copy, Clone)]
 struct Reg(usize);
 
+impl Display for Reg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 struct Computer {
     labels: Vec<usize>,
     instr: Vec<Instruction>,
@@ -71,6 +118,8 @@ impl Computer {
                 labels[label_id] = instr.len();
             }
         }
+
+        // instr[6] = Instruction::Load(7, Reg(15));
 
         Self { labels, instr }
     }
@@ -137,6 +186,47 @@ impl Computer {
                 if step_count > 5_000_000 {
                     count += 1;
                     break;
+                }
+            }
+        }
+
+        count
+    }
+
+    fn run_1048576(&self) -> usize {
+        let mut count = 0;
+        let mut previous = Vec::new();
+
+        for r0_start in 0..=65535 {
+            // print!("{r0_start:<5}:");
+            let mut val = 0;
+            for r1_start in 0..16 {
+                let mut regs = [0u16; 16];
+                regs[0] = r0_start;
+                regs[1] = r1_start;
+                let mut pc = 0;
+                let mut step_count = 0;
+                while pc < self.instr.len() {
+                    self.step(&mut pc, &mut regs);
+                    step_count += 1;
+                    if step_count > 5_000_000 {
+                        count += 1;
+                        break;
+                    }
+                }
+                // print!(" {}", if pc < self.instr.len() { "." } else { "*" });
+                val <<= 1;
+                if pc >= self.instr.len() {
+                    val |= 1;
+                }
+            }
+            previous.push(val);
+            if previous.len().is_multiple_of(2) {
+                let half = previous.len() / 2;
+                if previous[0..half] == previous[half..] {
+                    println!("Possible loop, period = {half}, count = {count}:");
+                    println!(" > {:?}", &previous[0..half]);
+                    println!(" > {:?}", &previous[half..]);
                 }
             }
         }
